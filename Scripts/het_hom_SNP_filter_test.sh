7cfe577c68f298ddb4e10bcbbf4d0ae1e82b391f
Oliver Stuart <oliver.stuart93@gmail.com>
2023-10-09 19:03:31 +1100

    config wrangle
    updated config file

---
+++ Scripts/het_hom_SNP_filter_test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Script will not source config file below
+# Already checked paths and whether script knows file is there
+# Not an alias issue
+# Not any other recognisable issue
+
+. ../shell.config
+
+echo $HOME_DIR
+echo $READ_DIR
